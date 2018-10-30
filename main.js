a019b0b38a4ba90f4314ad494b58327173cdfab3
Ethan Simpson <ethansown@gmail.com>
2018-10-30 15:36:31 -0500

    Almost done with site transition

---
+++ main.js
@@ -0,0 +1,6 @@
+var flipPrompt = document.getElementById('flip-site-prompt');
+var flipSiteElement = document.getElementById('flip-site-inner');
+function flipSite(){
+    flipSiteElement.style.transform = 'rotateX(180deg)';
+    console.log('I am running');
+}
