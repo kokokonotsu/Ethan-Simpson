c0eb8a977b355ed1488a1cf6f26c37854fb67633
Ethan Simpson <ethansown@gmail.com>
2018-10-30 19:16:56 -0500

    Made changes to the navbar

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 var flipPrompt = document.getElementById('flip-site-prompt');
+var searchInput = document.getElementById('searchInput');
 
 function flipSite(){
     document.getElementById('flip-site-inner').style.transform = 'rotateX(180deg)';
+    console.log('I am running');
+}
+
+function extendSearch(){
+    searchInput.style.width = '2in';
     console.log('I am running');
 }
